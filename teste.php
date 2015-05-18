1419e53c381aa5208aa78a37eb0596e0dddb6c90
ailton39 <ailton390@bol.com.br>
2015-05-18 16:51:47 -0300

    math training
    math training

---
+++ teste.php
@@ -0,0 +1,141 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Documento sem título</title>
+<style type="text/css">
+#res{ width:1000px; height:100px; font-family:Verdana, Geneva, sans-serif; color:#000; font-size:13px;
+font-weight:bold;}
+
+</style>
+
+</head>
+
+<body>
+<center>
+<h3>Respostas corrigidas</h3>
+<hr>
+<div id="tudo">
+
+<div id="res">
+ <table width="1100" border="2px" bordercolor="#999999">
+<?php if(isset($_POST['res'])&& $_POST['res']== 'Respostas'){
+	@$a=$_POST['a']; //1
+	@$b=$_POST['b']; //2
+	@$c=$_POST['c']; //3
+	@$d=$_POST['d']; //4
+	@$e=$_POST['e']; //5
+	@$f=$_POST['f']; //6
+	@$g=$_POST['g']; //7
+	@$h=$_POST['h']; //8
+	@$i=$_POST['i']; //9
+	@$j=$_POST['j']; //10
+	@$l=$_POST['l']; //11
+	@$m=$_POST['m']; //12
+	@$n=$_POST['n']; //13
+	@$o=$_POST['o']; //14
+	@$p=$_POST['p']; //15
+	@$q=$_POST['q']; //16
+	@$r=$_POST['r']; //17
+	@$s=$_POST['s']; //18
+	@$t=$_POST['t']; //19
+	@$u=$_POST['u']; //20
+	
+	
+ 
+	if(isset($a)&& $a=='36'){
+		echo '<tr ><td>1)<font color="#33CC00"> Certo 6x6=36!</font></td>';
+		}else{echo '<tr><td>1)<font color="#FF0000"> Errado 6X6 ≠'.$a.' , 6X6=36 !</font></td>';}
+		
+		if(isset($e)&& $e=='81'){
+		echo '<td>5)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>5)<font color="#FF0000"> Errado 7X9 ≠'.$e.' , 7X9=48 !</font></td>';}
+		
+		if(isset($i)&& $i=='48'){
+		echo '<td>9)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>9)<font color="#FF0000"> Errado 7X9 ≠'.$i.' , 7X9=48 !</font></td>';}
+		
+		if(isset($n)&& $n=='48'){
+		echo '<td>13)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>13)<font color="#FF0000"> Errado 7X9 ≠'.$n.' , 7X9=48 !</font></td>';}
+		
+		if(isset($r)&& $r=='48'){
+		echo '<td>17)<font color="#33CC00">Certo 7X9=48!</font></td></tr>';
+		}else{echo '<td>17)<font color="#FF0000"> Errado 7X9 ≠'.$r.' , 7X9=48 !</font></td></tr>';}
+	
+		//coluna 1 fim
+		//coluna 2 inicio
+	if(isset($b)&& $b=='63'){
+		echo '<tr><td>2)<font color="#33CC00">Certo 7X9=63!</font></td>';
+		}else{echo '<tr><td>2)<font color="#FF0000"> Errado 7X9 ≠'.$b.' , 7X9=63 !</font></td>';}
+		
+		if(isset($f)&& $f=='48'){
+		echo '<td>6)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>6)<font color="#FF0000"> Errado 7X9 ≠'.$f.' , 7X9=48 !</font></td>';}
+		
+		if(isset($j)&& $j=='48'){
+		echo '<td>10)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>10)<font color="#FF0000"> Errado 7X9 ≠'.$j.' , 7X9=48 !</font></td>';}
+		if(isset($o)&& $o=='48'){
+		echo '<td>14)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>14)<font color="#FF0000"> Errado 7X9 ≠'.$o.' , 7X9=48 !</font></td>';}
+		
+		if(isset($s)&& $s=='48'){
+		echo '<td>18)<font color="#33CC00">Certo 7X9=48!</font></td></tr>';
+		}else{echo '<td>18)<font color="#FF0000"> Errado 7X9 ≠'.$s.' , 7X9=48 !</font></td></tr>';}
+			//coluna 2 fim
+			//coluna 3 inicio
+		
+	if(isset($c)&& $c=='35'){
+		echo '<tr><td>3)<font color="#33CC00">Certo 5X7=35!</font></td>';
+		}else{echo '<tr><td>3)<font color="#FF0000"> Errado 5X7 ≠'.$c.' , 5X7=35 !</font></td>';}
+		
+		if(isset($g)&& $g=='48'){
+		echo '<td>7)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>7)<font color="#FF0000"> Errado 7X9 ≠'.$g.' , 7X9=48 !</font></td>';}
+		 
+		 if(isset($l)&& $l=='48'){
+		echo '<td>11)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>11)<font color="#FF0000"> Errado 7X9 ≠'.$l.' , 7X9=48 !</font></td>';}
+		
+		if(isset($p)&& $p=='48'){
+		echo '<td>15)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>15)<font color="#FF0000"> Errado 7X9 ≠'.$p.' , 7X9=48 !</font></td>';}
+		if(isset($t)&& $t=='160'){
+		echo '<td>19)<font color="#33CC00">Certo 7X9=48!</font></td></tr>';
+		}else{echo '<td>19)<font color="#FF0000"> Errado 7X9 ≠'.$t.' , 7X9=48 !</font></td></tr>';}
+		
+	//coluna 3 fim
+			//coluna 4 inicio
+		
+	if(isset($d)&& $d=='135'){
+		echo '<tr><td>4)<font color="#33CC00">Certo 45X3=135!</font></td>';
+		}else{echo '<tr><td>4)<font color="#FF0000"> Errado 45X3 ≠'.$b.' , 45X3=135 !</font></td>';}
+			
+	if(isset($h)&& $h=='48'){
+		echo '<td>8)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>8)<font color="#FF0000"> Errado 7X9 ≠'.$h.' , 7X9=48 !</font></td>';}
+				
+	if(isset($m)&& $m=='48'){
+		echo '<td>12)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>12)<font color="#FF0000"> Errado 7X9 ≠'.$m.' , 7X9=48 !</font></td>';}
+		
+	if(isset($q)&& $q=='48'){
+		echo '<td>16)<font color="#33CC00">Certo 7X9=48!</font></td>';
+		}else{echo '<td>16)<font color="#FF0000"> Errado 7X9 ≠'.$q.' , 7X9=48 !</font></td>';}
+	
+	if(isset($u)&& $u=='268'){
+		echo '<td>20)<font color="#33CC00">Certo 7X9=48!</font></td></tr>';
+		}else{echo '<td>20)<font color="#FF0000"> Errado 7X9 ≠'.$u.' , 7X9=48 !</font></td></tr>';}
+	
+	//coluna 4 fim
+	
+							
+		}?>
+  
+</table>
+ </div>
+ </div>
+</center>
+</body>
+</html>
---
+++ teste2.php
@@ -0,0 +1,196 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Documento sem título</title>
+<style type="text/css">
+table{ font-family:Verdana, Geneva, sans-serif; font-size:13px; font-weight:bold;}
+</style>
+</head>
+
+<body>
+<center>
+<h3>Selecione as respostas corretas!</h3>
+<hr>
+<form action="teste.php" method="post"  enctype="multipart/form-data">
+<table width="1000" >
+  <tr>
+    <td align="center">1) 6x6 é igual a ?</td>
+    <td align="center">5) 9x9 é igual a ?</td>
+    <td align="center">9) 3x8 é igual a ?</td>
+    <td align="center">13) 9x6 é igual a ?</td>
+    <td align="center">17) 6x3 é igual a ?</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="a" value="25">25</td>
+    <td align="center"><input type="radio" name="e" value="81">81</td>
+    <td align="center"><input type="radio" name="i" value="32">32</td>
+    <td align="center"><input type="radio" name="n" value="68">68</td>
+    <td align="center"><input type="radio" name="r" value="18">18</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="a" value="36">36</td>
+    <td align="center"><input type="radio" name="e" value="72">72</td>
+    <td align="center"><input type="radio" name="i" value="24">24</td>
+    <td align="center"><input type="radio" name="n" value="57">57</td>
+    <td align="center"><input type="radio" name="r" value="11">11</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="a" value="47">47</td>
+    <td align="center"><input type="radio" name="e" value="27">27</td>
+    <td align="center"><input type="radio" name="i" value="22">22</td>
+    <td align="center"><input type="radio" name="n" value="48">48</td>
+    <td align="center"><input type="radio" name="r" value="16">16</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="a" value="56">56</td>
+    <td align="center"><input type="radio" name="e" value="81">81</td>
+    <td align="center"><input type="radio" name="i" value="36">36</td>
+    <td align="center"><input type="radio" name="n" value="54">54</td>
+    <td align="center"><input type="radio" name="r" value="22">22</td>
+  </tr>
+  <tr>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+  </tr>
+  <tr>
+    <td align="center">2) 7x9 é igual a ?</td>
+    <td align="center">6) 6x8 é igual a ?</td>
+    <td align="center">10) 8x4 é igual a ?</td>
+    <td align="center">14) 4x7 é igual a ?</td>
+    <td align="center">18) 4x9 é igual a ?</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="b" value="56">66</td>
+    <td align="center"><input type="radio" name="f" value="44">44</td>
+    <td align="center"><input type="radio" name="j" value="26">26</td>
+    <td align="center"><input type="radio" name="o" value="28">28</td>
+    <td align="center"><input type="radio" name="s" value="32">32</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="b" value="49">49</td>
+    <td align="center"><input type="radio" name="f" value="48">48</td>
+    <td align="center"><input type="radio" name="j" value="32">32</td>
+    <td align="center"><input type="radio" name="o" value="32">32</td>
+    <td align="center"><input type="radio" name="s" value="42">42</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="b" value="63">63</td>
+    <td align="center"><input type="radio" name="f" value="38">38</td>
+    <td align="center"><input type="radio" name="j" value="36">36</td>
+    <td align="center"><input type="radio" name="o" value="26">26</td>
+    <td align="center"><input type="radio" name="s" value="36">36</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="b" value="56">58</td>
+    <td align="center"><input type="radio" name="f" value="53">53</td>
+    <td align="center"><input type="radio" name="j" value="32">32</td>
+    <td align="center"><input type="radio" name="o" value="34">34</td>
+    <td align="center"><input type="radio" name="s" value="28">28</td>
+  </tr>
+  <tr>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+  </tr>
+  <tr>
+    <td align="center">3) 5x7 é igual a ?</td>
+    <td align="center">7) 3x9 é igual a ?</td>
+    <td align="center">11) 12x8 é igual a ?</td>
+    <td align="center">15) 9x11 é igual a ?</td>
+    <td align="center">19) 32x5 é igual a ?;</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="c" value="56">38</td>
+    <td align="center"><input type="radio" name="g" value="28">28</td>
+    <td align="center"><input type="radio" name="l" value="94">94</td>
+    <td align="center"><input type="radio" name="p" value="99">99</td>
+    <td align="center"><input type="radio" name="t" value="156">156</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="c" value="35">35</td>
+    <td align="center"><input type="radio" name="g" value="33">33</td>
+    <td align="center"><input type="radio" name="l" value="102">102</td>
+    <td align="center"><input type="radio" name="p" value="101">101</td>
+    <td align="center"><input type="radio" name="t" value="128">128</td>
+  <tr>
+   <td align="center"><input type="radio" name="c" value="37">37</td>
+    <td align="center"><input type="radio" name="g" value="24">24</td>
+    <td align="center"><input type="radio" name="l" value="98">98</td>
+    <td align="center"><input type="radio" name="p" value="89">89</td>
+    <td align="center"><input type="radio" name="t" value="160">160</td>
+  </tr>
+  <tr>
+    <td align="center"><input type="radio" name="c" value="42">42</td>
+    <td align="center"><input type="radio" name="g" value="27">27</td>
+    <td align="center"><input type="radio" name="l" value="96">96</td>
+    <td align="center"><input type="radio" name="p" value="97">97</td>
+    <td align="center"><input type="radio" name="t" value="162">162</td>
+  </tr>
+  <tr>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+  </tr>
+  <tr>
+    <td align="center">4) 45x3 é igual a?</td>
+    <td align="center">8) 43x7 é igual a ?</td>
+    <td align="center">12) 25x8 é igual a ?</td>
+    <td align="center">16) 9x32 é igual a ?</td>
+    <td align="center">20) 4x67 é igual a ?</td>
+  </tr>
+    <tr>
+    <td align="center"><input type="radio" name="d" value="145">145</td>
+    <td align="center"><input type="radio" name="h" value="301">301</td>
+    <td align="center"><input type="radio" name="m" value="250">250</td>
+    <td align="center"><input type="radio" name="q" value="296">296</td>
+    <td align="center"><input type="radio" name="u" value="266">266</td>
+  </tr>
+   <tr>
+    <td align="center"><input type="radio" name="d" value="125">125</td>
+    <td align="center"><input type="radio" name="h" value="345">345</td>
+    <td align="center"><input type="radio" name="m" value="225">225</td>
+    <td align="center"><input type="radio" name="q" value="278">278</td>
+    <td align="center"><input type="radio" name="u" value="268">268</td>
+  </tr>
+   <tr>
+    <td align="center"><input type="radio" name="d" value="160">160</td>
+    <td align="center"><input type="radio" name="h" value="289">289</td>
+    <td align="center"><input type="radio" name="m" value="210">210</td>
+    <td align="center"><input type="radio" name="q" value="288">288</td>
+    <td align="center"><input type="radio" name="u" value="322">322</td>
+  </tr>
+   <tr>
+    <td align="center"><input type="radio" name="d" value="135">135</td>
+    <td align="center"><input type="radio" name="h" value="325">325</td>
+    <td align="center"><input type="radio" name="m" value="200">200</td>
+    <td align="center"><input type="radio" name="q" value="256">256</td>
+    <td align="center"><input type="radio" name="u" value="188">278</td>
+  </tr>
+   <tr>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"><hr></td>
+    <td align="center"> <hr></td>
+  </tr>
+   <tr>
+    <td height="26" align="center"></td>
+    <td align="center">&nbsp;</td>
+    <td align="center"><input type="submit" name="res" value="Respostas"></td>
+    <td align="center">&nbsp;</td>
+       <td align="center">&nbsp;</td>
+  </tr>
+</table>
+</form>
+<hr>
+</center>
+</body>
+</html>
